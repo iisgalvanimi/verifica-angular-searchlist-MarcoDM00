7f9b685e3b8fa0fd5dedc0d916d14c8857c2d446
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-03-10 08:52:02 +0000

    finito :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Empty-Project';
+  valori:{testo:string, nascondi:boolean}[] = [
+    {testo:"0 - la banana", nascondi:false},
+    {testo:"1 - la fragola", nascondi:false},
+    {testo:"2 - il lampone", nascondi:false},
+    {testo:"3 - il melone", nascondi:false}
+  ];
+  input:string = "";
+
+  change() {
+    for (let i = 0; i < this.valori.length; i++) {
+      if (this.input == "") {
+        this.valori[i].nascondi = false;
+      } else {
+        if (!(this.valori[i].testo.search(this.input) != -1)) {
+          this.valori[i].nascondi = true;
+        }
+      }
+    }
+  }
 }
